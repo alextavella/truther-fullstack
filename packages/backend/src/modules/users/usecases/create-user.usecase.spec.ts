261c330f2b6d1302da1c80ef77d7c4d82215b05d
alextavella <alex.tavella@azos.com.br>
2025-02-13 18:28:56 -0300

    test: create user usecase

--- packages/backend/src/modules/users/usecases/create-user.usecase.spec.ts
+++ packages/backend/src/modules/users/usecases/create-user.usecase.spec.ts
@@ -1,10 +1,49 @@
-// import { faker } from '@faker-js/faker'
-// import { describe, expect, it } from 'vitest'
-// import { createNote } from './notes.usecase'
+import { UserEntity } from '@/domain/entity/user'
+import type { IUserRepository } from '@/repositories/user.repository'
+import { FakeUserRepository } from '@/tests/fakes/fake-users.repository'
+import { faker } from '@faker-js/faker'
+import { UnprocessableEntity } from 'http-errors'
+import { beforeEach, describe, expect, it } from 'vitest'
+import {
+  CreateUsersUseCase,
+  type ICreateUserUseCase,
+} from './create-user.usecase'
 
-// describe('createNote', () => {
-//   it('should call methods with correct params', async () => {
-//     const note = await createNote({ message: faker.string.sample() })
-//     expect(note.id).toBeDefined()
-//   })
-// })
+describe(CreateUsersUseCase.name, () => {
+  let sut: ICreateUserUseCase
+  let usersRepository: IUserRepository
+
+  beforeEach(() => {
+    usersRepository = new FakeUserRepository()
+    sut = new CreateUsersUseCase(usersRepository)
+  })
+
+  it('should call methods with correct params', async () => {
+    // Arrange
+    const user = UserEntity.newUser({
+      name: faker.internet.username(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    })
+    // Act
+    const result = await sut.execute(user)
+    // Assert
+    expect(result.id).toBeDefined()
+    expect(result.name).toEqual(user.name)
+    expect(result.email).toEqual(user.email)
+    expect(result.password).toBeDefined()
+  })
+
+  it('should throw 422 when user already exists', async () => {
+    // Arrange
+    const user = UserEntity.newUser({
+      name: faker.internet.username(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    })
+    // Act
+    usersRepository.create(user)
+    // Assert
+    await expect(sut.execute(user)).rejects.toThrowError(UnprocessableEntity)
+  })
+})
---
+++ packages/backend/src/tests/fakes/fake-users.repository.ts
@@ -0,0 +1,18 @@
+import type { NewUser, User } from '@/domain/entity/user'
+import type { IUserRepository } from '@/repositories/user.repository'
+import { randomUUID } from 'crypto'
+
+export class FakeUserRepository implements IUserRepository {
+  readonly _data = new Map<string, User>()
+
+  async create(data: NewUser): Promise<User> {
+    const id = randomUUID()
+    const user: User = { ...data, id: 1, role: data?.role ?? 'customer' }
+    this._data.set(id, user)
+    return user
+  }
+
+  async findByEmail(email: string): Promise<User | void> {
+    return Array.from(this._data.values()).find(user => user.email === email)
+  }
+}
