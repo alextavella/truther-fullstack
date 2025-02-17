627ec0ecccfa8b9d1d81fedd43c7e190fda11165
alextavella <alex.tavella@azos.com.br>
2025-02-17 19:51:05 -0300

    chore: create heading component

---
+++ packages/mobile/src/components/heading/styles.ts
@@ -0,0 +1,12 @@
+import { fontFamily } from '@/styles/font-family'
+import { StyleSheet } from 'react-native'
+
+export const s = StyleSheet.create({
+  text: {
+    fontFamily: fontFamily.bold,
+    fontSize: 24,
+    marginBottom: 16,
+    letterSpacing: -0.8,
+    lineHeight: 34,
+  },
+})
