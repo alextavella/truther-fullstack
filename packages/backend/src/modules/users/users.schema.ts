1b9cf9b5fda4ec429cb5b89a446250eee9a25046
alextavella <alex.tavella@azos.com.br>
2025-02-18 18:00:14 -0300

    refactor: update getting user response

--- packages/backend/src/modules/users/users.schema.ts
+++ packages/backend/src/modules/users/users.schema.ts
@@ -69,6 +69,9 @@ export const listUserSchema = {
 const getBodySchema = userEntitySchema.pick({ email: true, password: true })
 const getResultSchema = z.object({
   access_token: z.string(),
+  uid: z.number(),
+  email: z.string(),
+  role: z.enum(userRoles).nullable(),
 })
 export const getUserSchema = {
   schema: {
