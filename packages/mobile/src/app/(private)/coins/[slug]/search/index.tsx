1dc23b279cd90fa9cf5b659baae3bc4f3a6bb37f
alextavella <alex.tavella@azos.com.br>
2025-02-20 14:29:28 -0300

    chore: create coin market view

--- packages/mobile/src/app/(private)/coins/[slug]/search/index.tsx
+++ packages/mobile/src/app/(private)/coins/[slug]/search/index.tsx
@@ -1,8 +1,11 @@
+import { AppBar } from '@/components/app-bar'
 import { ListCoin } from '@/components/coins/list-item'
+import { useRedirect } from '@/hooks/useRedirect'
 import { useSearchCoins } from '@/hooks/useSearchCoins'
 import { colors } from '@/styles/colors'
 import { useLocalSearchParams } from 'expo-router'
 import React from 'react'
+import { StyleSheet, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 
 type ParamsProps = {
@@ -12,15 +15,18 @@ type ParamsProps = {
 export default function SearchCoins() {
   const { slug } = useLocalSearchParams<ParamsProps>()
   const [coins] = useSearchCoins(slug)
+  const { goToDetailsCoins } = useRedirect()
 
   return (
-    <>
+    <View style={s.container}>
+      <AppBar title="Tokens" />
       <FlatList
         style={{ width: '100%' }}
         data={coins}
         keyExtractor={item => item.id}
         renderItem={({ index, item }) => (
           <ListCoin.Root
+            onPress={() => goToDetailsCoins(item.id)}
             style={{
               backgroundColor:
                 index % 2 === 0 ? colors.gray[200] : colors.gray[100],
@@ -31,6 +37,13 @@ export default function SearchCoins() {
           </ListCoin.Root>
         )}
       />
-    </>
+    </View>
   )
 }
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+})
