1011979675b6ecfc82963c1ebe5e01056c5ab8c9
alextavella <alex.tavella@azos.com.br>
2025-02-20 18:51:29 -0300

    refactor: remove react-query

--- packages/mobile/src/providers/AppProvider.tsx
+++ packages/mobile/src/providers/AppProvider.tsx
@@ -1,11 +1,5 @@
-import { queryClient } from '@/lib/queryClient'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from './SessionProvider'
 
 export function AppProvider({ children }: React.PropsWithChildren) {
-  return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </SessionProvider>
-  )
+  return <SessionProvider>{children}</SessionProvider>
 }
