566c2765307c1bf9980d8f740793d1d9b10830bb
alextavella <alex.tavella@azos.com.br>
2025-02-24 10:54:28 -0300

    chore: set imports

--- packages/mobile/.eslintrc.js
+++ packages/mobile/.eslintrc.js
@@ -2,6 +2,11 @@
 module.exports = {
   extends: 'expo',
   ignorePatterns: ['/dist/*'],
+  import: [
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
   rules: {
     'react-hooks/exhaustive-deps': 'error',
   },
